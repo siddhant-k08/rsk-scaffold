f3d0ded942222479078d4bc8ead882f4c05b3f57
Carlos Rosado <carlos.rosado@rootstocklabs.com>
2025-08-21 10:18:55 -0600

    Update Title component

--- packages/nextjs/components/home/Title.tsx
+++ packages/nextjs/components/home/Title.tsx
@@ -3,9 +3,8 @@ import React from "react";
 function Title() {
   return (
     <h1 className="flex font-bold text-[64px] gap-2 text-black">
-      <span className="bg-white p-1">Scaffold</span>
-      <span className="bg-brand-green p-1"> ETH</span>
       <span className="bg-brand-orange p-1">Rootstock</span>
+      <span className="bg-white p-1">Scaffold</span>
     </h1>
   );
 }
